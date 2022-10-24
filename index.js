cf20d74af7d7242b4c84298f1c22356f909a352b
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-10-25 10:07:26 +1100

    html layout added

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById('author').textContent = `By: ${data.user.name}`
+    })
